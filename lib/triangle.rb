abd680cb1105cd35331fc15a11078a4ab1dbcc45
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:25:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ def initialize(side_1, side_2, side_3)
 end
 
 def kind
-  triange_real?
+  triangle_real?
     if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
