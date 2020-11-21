91b953b82497406ef46ebc41b487ef0e0f67e532
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:13:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,24 @@
 require 'pry'
 class Triangle
+
 class TriangleError < StandardError
+
 attr_accessor :side_1, :side_2, :side_3
 
 def initialize(side_1, side_2, side_3)
+  @side_1 = side_1
+  @side_2 = side_2
+  @side_3 = side_3
+end
+
+def kind
+  if @side_1 == @side_2 && @side_2 == @side_3
+    return :equilateral
+  else
+    false
+  end
+
+end
 
 
 
