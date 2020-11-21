544d1b4fb2fcae3866462018ff49195ffa15ee4c
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:21:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ end
 def kind
     if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
-    elsif a == b || b == c || a == c
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
       :isosceles
     else
       :scalene
