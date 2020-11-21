4c0cb994e51f41dacdbcd879cde4b24888bbc840
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:18:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
 
 class TriangleError < StandardError
 
-attr_accessor :side_1, :side_2, :side_3
+attr_reader :side_1, :side_2, :side_3
 
 def initialize(side_1, side_2, side_3)
   @side_1 = side_1
@@ -12,7 +12,7 @@ def initialize(side_1, side_2, side_3)
 end
 
 def kind
-    if side_1 == side_2 && side_2 == side_3
+    if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     elsif a == b || b == c || a == c
       :isosceles
