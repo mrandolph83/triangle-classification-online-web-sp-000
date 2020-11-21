a061e07ffbc823238455b707807abebad54faa0f
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:06:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,5 @@
+require 'pry'
 class Triangle
-  # write code here
-class Triangle_error < StandardError
-
   def initialize(side_1, side_2, side_3)
     @triangle_sides = []
     @triangle_sides << side_1
@@ -9,12 +7,34 @@ class Triangle_error < StandardError
     @triangle_sides << side_3
   end
 
-  def kind
+  def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
 
-    if @triangle_sides[0] == @triangle_sides[1] && @triangle_sides[2] == @triangle_sides[1]
-      return :equilateral
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
+  end
+
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
     end
   end
 end
+
+class TriangleError < StandardError
 
 end
