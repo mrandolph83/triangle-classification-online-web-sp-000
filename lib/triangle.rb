192703b1e74a0c2614f837bc723308ae7558efba
Matt Smith <mrandolph83@gmail.com>
2020-11-21 20:04:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,6 @@ class Triangle_error < StandardError
     if @triangle_sides[0] == @triangle_sides[1] && @triangle_sides[2] == @triangle_sides[1]
       return :equilateral
     end
-
   end
 
 end
